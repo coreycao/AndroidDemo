ede5405aeb52a26222758d783344c8e3f3ccd1ae
Corey <caosanyang@gmail.com>
2017-09-13 09:36:07 +0800

    Add status bar demo.

--- Basic/app/src/main/java/com/corey/basic/StatusBarActivity.java
+++ Basic/app/src/main/java/com/corey/basic/StatusBarActivity.java
@@ -15,37 +15,79 @@ import android.view.Window;
  */
 
 public class StatusBarActivity extends AppCompatActivity {
+
+    View decorView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bar);
-        hideStatusBar();
+        decorView = getWindow().getDecorView();
 //        hideActionBar();
-
+//        hideStatusBar();
+//        opticalStatusBar();
+//        hideNaviBar();
+//        opticalNaviBar();
     }
-
-    // 隐藏状态栏
-    private void hideStatusBar() {
-        View decorView = getWindow().getDecorView();
-        int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(option);
-    }
-
 
     // 隐藏 actionBar
     private void hideActionBar() {
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
+    }
+
+    // 隐藏状态栏
+    private void hideStatusBar() {
+        int option = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if (decorView != null) {
+            decorView.setSystemUiVisibility(option);
+        }
     }
 
     // 透明状态栏
     private void opticalStatusBar() {
-        if (Build.VERSION.SDK_INT >= 21) {
-            View decorView = getWindow().getDecorView();
-            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        if (Build.VERSION.SDK_INT >= 21 && decorView != null) {
             decorView.setSystemUiVisibility(option);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    // 隐藏导航栏
+    private void hideNaviBar() {
+        int option = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        if (decorView != null) {
+            decorView.setSystemUiVisibility(option);
+        }
+    }
+
+    // 透明导航栏
+    private void opticalNaviBar() {
+        int option = View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        if (Build.VERSION.SDK_INT >= 21 && decorView != null) {
+            decorView.setSystemUiVisibility(option);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus && Build.VERSION.SDK_INT >= 19) {
+            if (decorView != null) {
+                decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
         }
     }
 }
